156e4f4124a11bf99d79d516aa95d271801f444d
TimmyWei <dancingbus9000@gmail.com>
2020-01-22 23:51:04 +0800

    first commit

---
+++ 011(留言分析程式).py
@@ -0,0 +1,12 @@
+data = []
+with open('reviews.txt', 'r') as f:
+	for line in f:
+		data.append(line)
+		if len(data) % 1000 == 0:
+			print(len(data))
+print('檔案讀取完畢，共', len(data), '筆資料')
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+print('平均留言長度為: ', sum_len / len(data))
+	
