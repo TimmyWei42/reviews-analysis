0dbe9be8a28d8c552d15a971375e54d981bd13df
TimmyWei <dancingbus9000@gmail.com>
2020-01-23 00:07:13 +0800

    add filtering object

--- 011(留言分析程式).py
+++ 011(留言分析程式).py
@@ -1,4 +1,6 @@
 data = []
+hund = []
+good = []
 with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
@@ -8,5 +10,18 @@ print('檔案讀取完畢，共', len(data), '筆資料')
 sum_len = 0
 for d in data:
 	sum_len = sum_len + len(d)
+	if len(d) < 100:
+		hund.append(d)
 print('平均留言長度為: ', sum_len / len(data))
+
+print(hund[0])
+print('--------------')
+print(hund[1])
+
+for d in data:
+	if d in 'good':
+		good.append(d)
+print(good[0])
+print(good[1])
+
 	
